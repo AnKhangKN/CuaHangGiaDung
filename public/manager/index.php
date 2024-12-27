51cde7b0aabcbdc90b7e6402bc0c556209c8d3c9
PVH1512 <hieu02617@gmail.com>
2024-12-27 15:01:56 +0700

    f

--- public/manager/index.php
+++ public/manager/index.php
@@ -2,7 +2,7 @@
 
     include($_SERVER['DOCUMENT_ROOT'] . "/CuaHangDungCu/config/connect.php");
 
-    include '../../app/views/manager/includes/header.php';
+    include($_SERVER['DOCUMENT_ROOT'] . "/CuaHangDungCu/app/views/manager/includes/header.php");
 
     if (!isset($_GET['page'])) {
         include '../../app/views/manager/TrangChu.php';
@@ -10,6 +10,6 @@
         include "../../app/views/manager/" . $_GET['page'] . ".php";
     }
     
-    include '../../app/views/manager/includes/footer.php';
+    include($_SERVER['DOCUMENT_ROOT'] . "/CuaHangDungCu/app/views/manager/includes/footer.php");
 
 ?>
