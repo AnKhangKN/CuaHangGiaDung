2f3df5b953e90180b55a0690cf84e11eb802f8e2
AnKhang <minecraftcopyright1302@gmail.com>
2025-05-05 16:58:03 +0700

    fsdf

--- public/index.php
+++ public/index.php
@@ -3,35 +3,35 @@
 session_start();
 ob_start();
 
-include 'C:/xampp/htdocs/CuaHangDungCu/config/session.php';
-include 'C:/xampp/htdocs/CuaHangDungCu/config/connectdb.php'; 
+include '../config/session.php';
+include '../config/connectdb.php'; 
 
 $conn = connectBD(); 
 
-include (__DIR__ . '../../app/views/customer/include/header.php');
+include '../app/views/customer/include/header.php';
 if (isset($_GET['page'])) {
     $page = basename($_GET['page']);
 } else {
     $page = 'home';
 }
 
-$pagePath = __DIR__ . "../../app/views/customer/{$page}.php";
+$pagePath =  "../app/views/customer/{$page}.php";
 
 // Xử lý các trang yêu cầu tham số bổ sung
 if ($page === 'details' && isset($_GET['id'])) {
     $productId = intval($_GET['id']);
-    $pagePath = __DIR__ . '../../app/views/customer/details.php';
+    $pagePath = '../app/views/customer/details.php';
 }
 
 if ($page === 'details_bill' && isset($_GET['idBill'])) {
     $billId = intval($_GET['idBill']);
-    $pagePath = __DIR__ . '../../app/views/customer/details_bill.php';
+    $pagePath =  '../app/views/customer/details_bill.php';
 }
 
 // Kiểm tra xác thực cho trang thông tin người dùng
 if ($page === 'information') {
     if (!isset($_SESSION['user_id'])) {
-        header('Location: /CuaHangDungCu/app/views/others/login.php');
+        header('Location: /CuaHangGiaDung/app/views/others/login.php');
         exit;
     }
 }
@@ -41,11 +41,11 @@ if (file_exists($pagePath)) {
     include($pagePath);
 } else {
     // Chuyển hướng đến trang 404 nếu tệp không tồn tại
-    header('Location: /CuaHangDungCu/app/views/customer/404.php');
+    header('Location: /CuaHangGiaDung/app/views/customer/404.php');
     exit;
 }
 
-include (__DIR__ . '../../app/views/customer/include/footer.php');
+include  '../app/views/customer/include/footer.php';
 
 mysqli_close($conn);
 
